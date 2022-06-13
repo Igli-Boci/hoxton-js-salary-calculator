31622cbff0f062b6660d3e29d75d59d4a92bfe01
Igli-Boci <igliboci10@gmai.com>
2022-06-13 22:44:03 +0200

    Adding js for under 80eu wage

---
+++ index.js
@@ -0,0 +1,7 @@
+//  Less than 80 eu (3d world country)
+let salary1 = 80;
+const pension = salary1 * 0.05;
+const tax1 = salary1 - pension;
+let taxUnder80Eu = 0;
+let totalSalary = tax1;
+console.log("I'm sorry to inform you that with " + totalSalary + " EUR you can barely breathe let alone live a normal life.");
